0a5aa2e9164a7c71bee1fba3f744f6faf9f05b5d
Razfar14 <rajarangkuty14@gmail.com>
2026-06-02 18:38:47 +0700

    menunjuk ke sebuah nama file

--- StreamReader.cpp
+++ StreamReader.cpp
@@ -7,5 +7,13 @@ int main(){
     //deklarasi variabel baris untuk menyipan data string
     string baris;
 
+    //membuka file dalam mode menulis
+    ofstream outfile;
+   
+    //menunjuk ke sebuah nama file
+    outfile.open("contohfile.txt");
+
+    cout << ">= Menulis file, \'q\'untuk keluar " <<endl;
+
     
      
